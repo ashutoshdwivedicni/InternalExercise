87379389243fc5c8c844dad1ead49d26efd5888f
ashutoshdwivedicni <ashutoshdwivedi.cni@gmail.com>
2025-09-07 21:47:14 +0530

    Create Calculator.py
    Check Program

---
+++ Calculator.py
@@ -0,0 +1,13 @@
+Operand1=int(input("Enter the Operand-1 Value:")
+Operand2=int(input("Enter the Operand-2 Value:")
+Operator=input("Enter the Operator +,-,*,/:")
+If Operator==+:
+  print(Operand1+Operand2)
+Elif Operator==-:
+  print(Operand1-Operand2)
+Elif Operator==*:
+  print(Operand1*Operand2)
+  Elif Operator==/:
+  print(Operand1/Operand2)
+else:
+print("None")
